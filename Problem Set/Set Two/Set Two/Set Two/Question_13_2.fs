3eab436d9bcbd916840e94c64d4cebd8ade467bc
Hector R <github.hector1@gmail.com>
2020-03-11 18:39:29 -0400

    final commit, fixed bug where when using parenthesis the code would cut of some of the remaining tokens.

--- Problem Set/Set Two/Set Two/Set Two/Question_13_2.fs
+++ Problem Set/Set Two/Set Two/Set Two/Question_13_2.fs
@@ -13,7 +13,7 @@ type 'a parseTree =
     | E_branch of tree: 'a parseTree
     
 let eat terminal = function
-    | x::xs when x = terminal -> Some(xs)
+    | terminal::xs -> Some(xs)
     | _ -> None     
 let rec E = function
     | [] -> None 
@@ -28,9 +28,6 @@ let rec E = function
                  let tmpHolder = tail |> T
                  let (t_tree, tail) = if tmpHolder <> None then tmpHolder.Value else (Br_Id, [EOF])
                  Some(Br_Sub(e_tree,t_tree), tail)
-    | [x] -> let tmpHolder = [x] |> F
-             let (f_tree, tail) = if tmpHolder <> None then tmpHolder.Value else (Br_Id, [])
-             Some(f_tree, tail)
     | x::xs -> let tmpHolder = (x::xs) |> T
                let (t_tree, tail) = if tmpHolder <> None then tmpHolder.Value else (Br_Id, [])
                Some(t_tree, tail)
@@ -58,7 +55,8 @@ and F = function
                     let tmpHolder = tail |> eat RPAREN
                     let tail = if tmpHolder <> None then tmpHolder.Value else [EOF]
                     Some(e_tree, tail)
-    | _ -> None
+    | RPAREN::xs -> E xs
+    | _ -> None 
 let errorHandler = function
      | None -> "Invalid Program Passed"
      | Some x -> match x with
@@ -69,8 +67,8 @@ let runner =
     let program2 = [ID;SUB;ID;MUL;ID;EOF]
     let program3 = [LPAREN;ID;SUB;ID;RPAREN;MUL;ID;EOF]
     printf "Is program: %A valid? "program1
-    program1 |> E |> errorHandler |> printf "%s \n"
+   // program1 |> E |> errorHandler |> printf "%s \n"
     printf "Is program: %A valid? "program2
-    program2 |> E |> errorHandler |> printf "%s \n"
+   // program2 |> E |> errorHandler |> printf "%s \n"
     printf "Is program: %A valid? "program3
     program3 |> E |> errorHandler |> printf "%s \n"
